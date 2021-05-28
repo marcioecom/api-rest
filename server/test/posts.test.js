18025de7c9e91a1a440621c0185e005bad92a127
Marcio Junior <marcio.mj70@gmail.com>
2021-05-28 09:46:23 -0300

    test V2

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,6 +1,5 @@
 const axios = require('axios')
 
-
 test('Should get posts', async function () {
   const response = await axios({
     url: 'http://localhost:3000/posts',
@@ -8,4 +7,8 @@ test('Should get posts', async function () {
   })
   const posts = response.data
   expect(posts).toHaveLength(3)
+  const [firstPost] = posts
+  console.log(firstPost)
+  expect(firstPost.id).toBe(1)
+  expect(firstPost.title).toBe('REST API: Metodos')
 })
